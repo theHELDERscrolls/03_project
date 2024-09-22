4197ec7e6c9fbcedcefacd85dc26091cf431397f
Helder M. V. R. <manuhelderruiz@gmail.com>
2024-09-22 21:36:19 +0200

    Building header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import "./Header.css";
 export const createHeader = () => {
   const header = document.querySelector("header");
   header.innerHTML = `
-  
+    <img id="logo" src="./src/assets/logo_copynterest.jpeg" alt="Copynterest logo">
+    <input type="search" id="search" name="search" placeholder="Search for images..." aria-label="Search">
   `;
 };
