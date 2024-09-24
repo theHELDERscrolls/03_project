8de6c61618ef44c10f284ebb1a240076c98aa8a5
Helder M. V. R. <manuhelderruiz@gmail.com>
2024-09-24 07:39:02 +0200

    change favicon; basic utils

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 export const createHeader = () => {
   const header = document.querySelector("header");
   header.innerHTML = `
-    <img id="logo" src="./src/assets/logo_copynterest.jpeg" alt="Copynterest logo">
+    <img id="logo" src="./src/assets/icon_copynterest.png" alt="Copynterest logo">
     <input type="search" id="search" name="search" placeholder="Search for images..." aria-label="Search">
   `;
 };
