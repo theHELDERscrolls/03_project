29e34d7e337a4b6380e89150911c1219af29a7ef
Helder M. V. R. <manuhelderruiz@gmail.com>
2024-09-25 07:06:59 +0200

    update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 //! RECUERDA: crea ".env" para no hacer pública la id de Unsplash.
 
-import { createHeader } from "./src/components/Header/Header";
+import { createHeader } from "./src/components/Navbar/Navbar";
 import { createMain } from "./src/components/Main/Main";
 import { createFooter } from "./src/components/Footer/Footer";
 
--- src/components/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import "./Header.css";
-
-export const createHeader = () => {
-  const header = document.querySelector("header");
-  header.innerHTML = `
-    <img id="logo" src="./src/assets/icon_copynterest.png" alt="Copynterest logo">
-    <input type="search" id="search" name="search" placeholder="Search for images..." aria-label="Search">
-  `;
-};
