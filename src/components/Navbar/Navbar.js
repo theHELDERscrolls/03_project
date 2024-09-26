3a0910b4b6351662f39ec317a964d4581669fdfc
Helder M. V. R. <manuhelderruiz@gmail.com>
2024-09-26 22:13:25 +0200

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,17 +3,22 @@ import "./Navbar.css";
 export const createHeader = () => {
   const header = document.querySelector("header");
   header.innerHTML = `
-    <img id="logo" src="src/assets/images/logo_copynterest.png" alt="Copynterest logo">
-    <nav>
-      <ul>
-        <li>
-          <a id="home_link" href="#">Home</a>
-        </li>
-        <li>
-          <a id="explore_link" href="#">Explore</a>
-        </li>
-      </ul>
-    </nav>
-    <input type="search" id="search" name="search" placeholder="Search for images..." aria-label="Search">
+        <img id="logo" src="src/assets/images/logo_copynterest.png" alt="Copynterest logo">
+        <nav>
+            <ul>
+                <li>
+                    <a id="home_link" href="#">Home</a>
+                </li>
+                <li>
+                    <a id="explore_link" href="#">Explore</a>
+                </li>
+            </ul>
+        </nav>
+        <input type="search" id="search" name="search" placeholder="Search for images..." aria-label="Search">
+        <div id="icons">
+            <img id="notification" src="./src/assets/icons/icon_notification.svg" alt="Notifications">
+            <img id="ld-btn" src="./src/assets/icons/icon_sun.svg" alt="Light mode">
+        </div>
+
   `;
 };
